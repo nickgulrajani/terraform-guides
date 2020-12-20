44fcc730de56106aea505cbb7945295e3efc2212
Nicholas Gulrajani <gulrajan@wwt.com>
2020-12-20 17:13:21 -0500

    WIP

--- infrastructure-as-code/aws-ec2-instance/main.tf
+++ infrastructure-as-code/aws-ec2-instance/main.tf
@@ -2,16 +2,28 @@ terraform {
   required_version = ">= 0.11.0"
 }
 
+terraform {
+  backend "remote" {
+    organization = "wwtatctrialaccount"
+
+    workspaces {
+      name = "awscloudservice"
+    }
+  }
+}
+
+
 provider "aws" {
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region}a"
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  availability_zone = var.aws_region
 
-  tags {
-    Name = "${var.name}"
+
+  tags = {
+    Name = "var.name"
   }
 }
--- infrastructure-as-code/aws-ec2-instance/variables.tf
+++ infrastructure-as-code/aws-ec2-instance/variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-2e1ef954"
+  default = "ami-07907c2fc6edcbd42"
 }
 
 variable "instance_type" {
